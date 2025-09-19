3fc0686b724da138ed08e70a04efffb738626fda
Eric Lackey <eric.lackey@gmail.com>
2025-09-19 15:02:20 -0500

    Initial commit

--- netsuite-json-field.js
+++ netsuite-json-field.js
@@ -1,56 +1,64 @@
 
+
+function getFieldValueInJsonFormat(fieldId) {
+
+    require(['N/currentRecord'], (currentRecord) => {
+    
+        const thisRecord = currentRecord.get();
+
+        var rawJson = thisRecord.getValue({fieldId: fieldId});
+
+        // If we are in view only mode the field value might not be accessible due to NS quirk
+        if (thisRecord.isReadOnly && !rawJson) {
+            rawJson = document.querySelector(
+                `div[data-field-name="${fieldId}"] span[data-nsps-type="field_input"]`
+            )?.innerText;
+        }
+
+        return JSON.parse(rawJson);
+        
+    });
+
+}
+
+function setOriginalFieldValue(val) {
+
+    console.log(val)
+
+    require(['N/currentRecord'], (currentRecord) => {
+        var thisRecord = currentRecord.get();
+
+        try {
+            const updatedJson = window.editor.get();
+            thisRecord.setValue({
+                fieldId: editorOptions.netsuiteFieldId,
+                value: JSON.stringify(updatedJson)
+            });
+        } catch (err) {
+            console.error("Invalid JSON", err);
+        }        
+
+    });
+
+}
+
 function loadEditor() {
 
-    document.getElementById('jsoneditor').style.width = editorOptions.width;
-    document.getElementById('jsoneditor').style.height = editorOptions.height;
-    document.getElementById('jsoneditor').style.border = editorOptions.border;
-    require(['N/currentRecord',"N"], (currentRecord, netsuite) => {
-        console.log(netsuite);
+    require(['N/currentRecord',"N"], (currentRecord) => {
         var thisRecord = currentRecord.get();
-        var recordId = thisRecord.id;
-        var thisField = thisRecord.getField({fieldId: editorOptions.netsuiteFieldId})
-
-        console.log(JSON.stringify(thisField));
-
-        if (!recordId) {
-            recordState = "create";
-        }
 
         var container = document.getElementById("jsoneditor");
         var options = {
             mode: thisRecord.isReadOnly ? "view" : "tree",
-            onChange: () => {
-                try {
-                    const updatedJson = window.editor.get();
-                    thisRecord.setValue({
-                        fieldId: editorOptions.netsuiteFieldId,
-                        value: JSON.stringify(updatedJson)
-                    });
-                } catch (err) {
-                    console.error("Invalid JSON", err);
-                }
-            }
+            onChange: () => setOriginalFieldValue(val)
         };
 
         window.editor = new JSONEditor(container, options);
-        console.log('are we still going?')
-        console.log(editorOptions.netsuiteFieldId)
+
         try {
-            console.log(thisRecord.getText({fieldId: editorOptions.netsuiteFieldId}));
-
-            var rawJson = {};
-
-            /*if (thisRecord.isReadOnly) {
-                rawJson = document.querySelector(
-                'div[data-field-name="custrecord_field_with_json_data"] span[data-nsps-type="field_input"]'
-                )?.innerText;
-            } else {
-            */
-                rawJson = thisRecord.getText({fieldId: editorOptions.netsuiteFieldId})
-            //}
-            //const rawJson = JSON.parse(thisRecord.getValue({fieldId: editorOptions.netsuiteFieldId}));
-            console.log(rawJson);
-            editor.set(JSON.parse(rawJson));
+            const jsonObject = getFieldValueInJsonFormat(editorOptions.netsuiteFieldId);
+            console.log(jsonObject);
+            editor.set(jsonObject);
         } catch(e) {
             console.log(e)
             editor.set({});
