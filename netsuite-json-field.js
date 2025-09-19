75be0cfb46a2658f8a1890956e21edbe95a4b6ff
Eric Lackey <eric.lackey@gmail.com>
2025-09-19 13:24:48 -0500

    Initial commit

--- netsuite-json-field.js
+++ netsuite-json-field.js
@@ -7,25 +7,12 @@ function loadEditor() {
     require(['N/currentRecord',"N"], (currentRecord, netsuite) => {
         console.log(netsuite);
         var thisRecord = currentRecord.get();
-
-        if (thisRecord.isReadOnly) {
-            mode = "view";
-        } else {
-        
-        }
-
-            const text = document.querySelector(
-            'div[data-field-name="custrecord_field_with_json_data"] span[data-nsps-type="field_input"]'
-            )?.innerText;
-
-        console.log(text);
-
         console.log(JSON.stringify(thisRecord));
         console.log(thisRecord.getField(editorOptions.netsuiteFieldId));
         console.log(thisRecord.getValue(editorOptions.netsuiteFieldId));
         var container = document.getElementById("jsoneditor");
         var options = {
-            mode: "tree",
+            mode: thisRecord.isReadOnly ? "view" : "tree",
             onChange: () => {
                 try {
                     const updatedJson = window.editor.get();
@@ -43,10 +30,13 @@ function loadEditor() {
         console.log(editorOptions.netsuiteFieldId)
         try {
             console.log(thisRecord.getValue({fieldId: editorOptions.netsuiteFieldId}));
+            const rawJson = document.querySelector(
+            'div[data-field-name="custrecord_field_with_json_data"] span[data-nsps-type="field_input"]'
+            )?.innerText;
 
 
 
-            const rawJson = JSON.parse(thisRecord.getValue({fieldId: editorOptions.netsuiteFieldId}));
+            //const rawJson = JSON.parse(thisRecord.getValue({fieldId: editorOptions.netsuiteFieldId}));
             console.log(rawJson);
             editor.set(rawJson);
         } catch(e) {
